b1d18bcccdec700298529c43696dc1aa613be088
Sven Anders Robbestad <robbestad@gmail.com>
2014-10-15 16:12:00 +0200

    php for heroku deployment

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<title>SVG Sprites</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <script src="js/svg4everybody.ie8.js"></script>
